76ae8e149c31ceda153e2dff54b695d775156df8
lwhitebird <lwhitebird@server.fake>
2014-01-25 00:14:16 -0800

    Refactored Pangrams and added more comprehensive unit testing.

--- Pangrams.py
+++ Pangrams.py
@@ -1,15 +1,23 @@
 import sys
-test_cases = open(sys.argv[1], 'r')
+test_cases = open(sys.argv[1])
 
-def missing(string):
-  alphabet = 'abcdefghijklmnopqrstuvwxyz'
-  for letter in string:
-    letterlower = letter.lower()
-    if letterlower in alphabet: alphabet = alphabet.replace(letterlower, '')
-  if alphabet: return alphabet
-  else: return 'NULL'
-  
+def return_string_or_null(string):
+    if string:  return string
+    else:       return 'NULL'
+
+def remove_if_present(letter, alphabet_subset):
+    if letter in alphabet_subset:
+        return alphabet_subset.replace(letter, '')
+    else: return alphabet_subset
+
+def needed_for_pangram(string):
+    alphabet_subset = 'abcdefghijklmnopqrstuvwxyz'
+    for letter in string:
+        lowercase_letter = letter.lower()
+        alphabet_subset = remove_if_present(lowercase_letter, alphabet_subset)
+    return return_string_or_null(alphabet_subset)
+
 for test in test_cases:
-  print missing(test)
-  
+    letters_needed_to_be_pangram(test)
+
 test_cases.close()
