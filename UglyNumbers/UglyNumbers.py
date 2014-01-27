66c3ead85942eb0c72a94858882070b611ffb983
lwhitebird <lwhitebird@server.fake>
2014-01-27 10:34:21 -0800

    Added UglyNumbers list of sums implementation and tests.

--- UglyNumbers/UglyNumbers.py
+++ UglyNumbers/UglyNumbers.py
@@ -3,22 +3,53 @@ test_cases = open(sys.argv[1], 'r')
 
 class OutOfRangeError(Exception): pass
 
-
-class UglyNumbers:
-    def __init__(self):
-        self = self.split('')
-
 def is_ugly(number):
-    truth = False
+    isugly = False
     single_digit_primes = [2, 3, 5, 7]
     for prime in single_digit_primes:
         if number % prime == 0:
-            truth = True
+            isugly = True
             break
-    return truth
+    return isugly
+
+def triplepossums(possums, index):
+    possumsplaceholder = list(possums)
+    for possum in possums:
+        newindex = len(possum) - index
+        summed = possum[:newindex] + '+' + possum[newindex:]
+        minused = possum[:newindex] + '+-' + possum[newindex:]
+        possumsplaceholder.append(summed)
+        possumsplaceholder.append(minused)
+    return possumsplaceholder
+
+def nueval(possum):
+    separatedpossum = possum.split('+')
+    tsum = 0
+    for sep in separatedpossum:
+        tsum += int(sep)
+    return tsum
+
+def evalpossums(possums):
+    possumplaceholder = list(possums)
+    for possum in possums:
+        possumindex = possums.index(possum)
+        possumplaceholder[possumindex] = nueval(possum)
+    return possumplaceholder
 
 def list_possible_sums(digitstring):
-    pass
-  
+    possible_sums = list()
+    possible_sums.append(digitstring)
+    for i in reversed(range(1, len(digitstring))):
+        possible_sums = triplepossums(possible_sums, i)
+    return evalpossums(possible_sums)
+
+for test in test_cases:
+    strippedtest = test.strip('\n')
+    if strippedtest:
+        totaluglys = 0
+        possums = list_possible_sums(strippedtest)
+        for possum in possums:
+            if is_ugly(possum): totaluglys += 1
+        print totaluglys
 
 test_cases.close()
--- UglyNumbers/UglyNumbersTest.py
+++ UglyNumbers/UglyNumbersTest.py
@@ -18,9 +18,9 @@ class KnownUglyValues(unittest.TestCase):
             self.assertFalse(result)
 
 class KnownSumValues(unittest.TestCase):
-# add more values to test
-    knownSumValues = ( (5670, (5670, 8, 18)), (0, (0)) )
-    knownNonSumValues = ( (5670, (6, 0, 1)), (0, (1, 2, -3)) )
+# add more values to test, output must be integer
+    knownSumValues = ( ('5670', (5670, 567, 567, 675, 72, 72, -665, -62, -62, 126, 63, 63, -14, 49, 49, 81, 18, 18, -59, 4, 4, 69, 6, 6, -71, -8, -8)), ('0', (0,)) )
+    knownNonSumValues = ( ('5670', (7, 0, 1)), ('0', (1, 2, -3)) )
 
     def testKnownSumsIncluded(self):
         """All possible sum values should return in sums list."""
@@ -33,12 +33,34 @@ class KnownSumValues(unittest.TestCase):
 
     def testKnownNonSumsNotIncluded(self):
         """No non-sum values should return in sums list."""
-        for knownNonSum in self.knownSumValues:
+        for knownNonSum in self.knownNonSumValues:
             digitstring = knownNonSum[0]
             sums = knownNonSum[1]
             possible_sums = UglyNumbers.list_possible_sums(digitstring)
             for sum in sums:
                 self.assertFalse(sum in possible_sums)
+#may not be necessary
+    def testSumIsInteger(self):
+        for knownNonSum in self.knownSumValues:
+            digitstring = knownNonSum[0]
+            possible_sums = UglyNumbers.list_possible_sums(digitstring)
+            for sum in possible_sums:
+                self.assertTrue(type(sum) == int)
+#clunkily added tests, need to be broken out into new classes
+    def test_evalpossums(self):
+        self.assertEqual(UglyNumbers.evalpossums(('1+2', '3+-4', '1')), [3, -1, 1])
+
+    def test_triplepossums(self):
+        print UglyNumbers.triplepossums(('123',), 1)
+        self.assertEqual(UglyNumbers.triplepossums(('12',), 1), ['12','1+2','1+-2'])
+        self.assertEqual(UglyNumbers.triplepossums(('123',), 1), ['123','12+3','12+-3'])
+        self.assertEqual(UglyNumbers.triplepossums(('1+23',), 1), ['1+23','1+2+3','1+2+-3'])
+
+    def testnueval(self):
+        self.assertEqual(UglyNumbers.nueval('1+2+3'), 6)
+        self.assertEqual(UglyNumbers.nueval('1+-2+3'), 2)
+        self.assertEqual(UglyNumbers.nueval('-1+2+3'), 4)
+        self.assertEqual(UglyNumbers.nueval('1+-0+3'), 4)
 
 class evaluate_if_ugly_bad_input(unittest.TestCase):
     def testNonInteger(self):
@@ -49,6 +71,7 @@ class evaluate_if_ugly_bad_input(unittest.TestCase):
         """evaluate_if_ugly should fail with input over 13 digits."""
         self.assertRaises(UglyNumbers.OutOfRangeError, UglyNumbers.is_ugly, 12345678901234)
 
+#might remove these altogether, may not be necessary
 class list_possible_sums_bad_input(unittest.TestCase):
     def testNonInteger(self):
         """list_possible_sums should fail with non-integer type input."""
@@ -56,7 +79,7 @@ class list_possible_sums_bad_input(unittest.TestCase):
 
     def testTooLarge(self):
         """evaluate_if_ugly should fail with input over 13 digits."""
-        self.assertRaises(UglyNumbers.OutOfRangeError, UglyNumbers.list_possible_sums, 12345678901234)
+        self.assertRaises(UglyNumbers.OutOfRangeError, UglyNumbers.list_possible_sums, '12345678901234')
 
 
 if __name__ == "__main__":
